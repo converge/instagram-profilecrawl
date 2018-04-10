9679bdcce5fbddcd0f4cd2a879e1b4f85fb181a6
converge <joaovanzuita@me.com>
2018-04-10 00:42:07 -0300

    crawl from csv file

--- crawl_profile.py
+++ crawl_profile.py
@@ -6,7 +6,7 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from util.cli_helper import get_all_user_names
+from util.cli_helper import get_all_user_from_file
 from util.extractor import extract_information
 
 chrome_options = Options()
@@ -17,12 +17,13 @@ chrome_options.add_argument('--headless')
 chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en-US'})
 browser = webdriver.Chrome('./assets/chromedriver', chrome_options=chrome_options)
 
-# makes sure slower connections work as well        
+# makes sure slower connections work as well
 print ("Waiting 10 sec")
 browser.implicitly_wait(10)
 
 try:
-  usernames = get_all_user_names()
+  # usernames = get_all_user_names()
+  usernames = get_all_user_from_file()
 
   for username in usernames:
     print('Extracting information from ' + username)
@@ -30,13 +31,13 @@ try:
 
     with open('./profiles/' + username + '.json', 'w') as fp:
       json.dump(information, fp)
-                                                     
+
     print ("Number of users who commented on his/her profile is ", len(user_commented_list),"\n")
-    file = open("./profiles/" + username + "_commenters.txt","w") 
+    file = open("./profiles/" + username + "_commenters.txt","w")
     for line in user_commented_list:
       file.write(line)
       file.write("\n")
-    file.close()     
+    file.close()
     print ("\nFinished. The json file and nicknames of users who commented were saved in profiles directory.\n")
 
 except KeyboardInterrupt:
--- util/cli_helper.py
+++ util/cli_helper.py
@@ -1,15 +1,22 @@
 """Util functions for the script"""
 import sys
+import csv
 
-def get_all_user_names():
-  """Returns all the usernames given as parameter"""
-  usernames = []
 
-  #display provide username
-  if len(sys.argv) < 2:
-    sys.exit('- Please provide at least one username!\n')
+def get_all_user_from_file():
+    """Returns all the usernames given as parameter"""
+    usernames = []
 
-  for username in sys.argv[1:]:
-    usernames.append(username)
+    # display provide username
+    if len(sys.argv) < 2:
+        sys.exit('- Please provide at least one username!\n')
 
-  return usernames
+    for filename in sys.argv[1:]:
+        with open(filename, 'r') as file:
+            reader = csv.reader(file)
+            for user in reader:
+                user = str(user)
+                user = user[2:-2]
+                usernames.append(user)
+
+    return usernames
